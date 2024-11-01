7f77ac4252b7004972409fc8e80fa86d885b559f
Mohamed Adel <76179743+mohamedadel29@users.noreply.github.com>
2024-11-01 22:54:30 +0200

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -40,6 +40,13 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 		
+		p = new Person();
+		p.setId("4");
+		p.setAge(23);
+		p.setFirstName("webhook");
+		p.setLastName("testing");
+		persons.add(p);
+		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
